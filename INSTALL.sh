89fa75caba930bdbbca32a827a81e1fd425f8fac
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2021-02-27 13:51:48 +0530

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# For the command terminal, copy-paste FROM here
+
+mkdir ~/.FreeCAD/Macro/AeroFoil_UI_Files
+wget --no-check-certificate -P ~/.FreeCAD/Macro https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil.FCMacro
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_1.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2a.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2b.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2c.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2d.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2d1a.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2d1b.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_2cd2.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Dialog_3.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_Math_Functions_Box.ui
+wget --no-check-certificate -P ~/.FreeCAD/Macro/AeroFoil_UI_Files https://raw.githubusercontent.com/melwyncarlo/AeroFoil/main/AeroFoil_UI_Files/AeroFoil_mfb_img.gif
+
+
+# For the command terminal, copy-paste TO here
