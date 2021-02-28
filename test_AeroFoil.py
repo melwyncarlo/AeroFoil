d9140643a4a005fee3d46ff3dcd89f68c573612d
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2021-02-28 19:08:30 +0530

    Update test_AeroFoil.py

--- test_AeroFoil.py
+++ test_AeroFoil.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_main():
-	test_proc = subprocess.run(['sh', 'if freecad AeroFoil.FCMacro & then echo "True"; kill $!; else echo "False"; fi'])
+	test_proc = subprocess.call(['sh', 'if freecad AeroFoil.FCMacro & then echo "True"; kill $!; else echo "False"; fi'])
 
